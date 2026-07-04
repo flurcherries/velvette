8f91083cc5c6a98d3dc5cdddad12b84497a4ad67
flurcherries <flurcherries@gmail.com>
2026-07-04 15:04:54 +0000

    Convert project to Next.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 module.exports = {
-=======
-export default {
->>>>>>> a495fc4302063088222f5193d46e301c09343f43
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-<<<<<<< HEAD
 };
-=======
-}
->>>>>>> a495fc4302063088222f5193d46e301c09343f43
